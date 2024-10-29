8d8b2e3447b4d512fd8154de35024263d9539203
Waptech <abdulfataiyakub07@gmail.com>
2024-10-29 20:42:04 +0100

    next auth login route

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,53 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "Your username" },
+                password: { label: "Password", type: "password", placeholder: "Your password" }
+            },
+            async authorize(credentials) {
+                const res = await fetch('https://trf-store-api.vercel.app/api/v1/auth/token', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        username: credentials.username,
+                        password: credentials.password,
+                    }),
+                });
+
+                const user = await res.json();
+
+                if (res.ok && user.access_token) {
+                    return { accessToken: user.access_token };
+                } else {
+                    throw new Error(user.message || user.detail || 'Login failed');
+                }
+            },
+        }),
+    ],
+    pages: {
+        signIn: '/login',
+    },
+    session: {
+        strategy: 'jwt',
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.accessToken = user.accessToken;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            session.accessToken = token.accessToken;
+            return session;
+        },
+    },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
