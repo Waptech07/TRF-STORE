5a48fab5c8333d741423603099dc5959273f1514
Waptech <abdulfataiyakub07@gmail.com>
2024-10-30 14:50:31 +0100

    add max screen height

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { FaUser, FaLock, FaShoePrints, FaTshirt } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-blue-500 to-purple-600 relative">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen max-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-br from-blue-500 to-purple-600 relative">
       <main className="flex flex-col gap-8 row-start-2 justify-center items-center">
         <h1 className="text-5xl font-serif text-white p-5 shadow-lg text-center">
           TRF - The Right Fit
